0368b6e472506f279496979dc17132f64102b638
André Rosengren <andree_@hotmail.com>
2018-03-05 12:14:34 +0100

    db anslutning

--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
-var http = require ('http');
-var url = require ('url');
-var fs = require('fs');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var mongoose = require("mongoose");
 
-
-
-
-
-http.createServer(function (req, res) {
-  var path = url.parse(req.url).pathname;
-  switch (path) {
-    case '/':
+http
+  .createServer(function(req, res) {
+    var path = url.parse(req.url).pathname;
+    switch (path) {
+      case "/":
         res.writeHead(200, {
-          'Content-Type': 'text/html'
+          "Content-Type": "text/html"
         });
-        res.write('Test message');
+        res.write("Test message");
         res.end();
         break;
-      case '/index.html':
+      case "/index.html":
         fs.readFile(__dirname + path, function(error, data) {
-          if(error) {
+          if (error) {
             res.writeHead(404);
             res.write(error);
             res.end();
           } else {
             res.writeHead(200, {
-              'Content-Type': 'text/html'
+              "Content-Type": "text/html"
             });
             res.write(data);
             res.end();
@@ -33,10 +31,21 @@ http.createServer(function (req, res) {
         break;
       default:
         res.writeHead(404);
-        res.write('Shit....')
+        res.write("Shit....");
         res.end();
         break;
+    }
+  })
+  .listen(8080);
+
+mongoose.connect(
+  "mongodb://admin:admin@ds023373.mlab.com:23373/allergikartandb",
+  {
+    useMongoClient: true
+  },
+  function(error) {
+    if (!error) {
+      console.log("databas funkar");
+    }
   }
-
-
-}).listen(8080);
+);
