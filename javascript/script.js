bf5333b7ad88ede24120f3d39c85844f6385889e
emmaindal <emma.indahl@gmail.com>
2018-03-06 11:11:04 +0100

    fixat mapparna

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".button-collapse").sideNav();
-	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-	$('.modal').modal();
-
-	$('select').material_select();
-});
