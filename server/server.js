cc4844b20e55cd541f099ba47403f36a8fdbb3ae
Sebastian Jakobsson <sebastianjakobsson@sebastians-air.clients.internal.mah.se>
2018-03-06 11:01:18 +0100

    sdfghj

--- server/server.js
+++ server/server.js
@@ -1,42 +1,23 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
+var path = require('path')
 var mongoose = require("mongoose");
+const express = require('express')
+const app = express()
 
-http
-  .createServer(function(req, res) {
-    var path = url.parse(req.url).pathname;
-    switch (path) {
-      case "/":
-        res.writeHead(200, {
-          "Content-Type": "text/html"
-        });
-        res.write("Test message");
-        res.end();
-        break;
-      case "/index.html":
-        fs.readFile(__dirname + path, function(error, data) {
-          if (error) {
-            res.writeHead(404);
-            res.write(error);
-            res.end();
-          } else {
-            res.writeHead(200, {
-              "Content-Type": "text/html"
-            });
-            res.write(data);
-            res.end();
-          }
-        });
-        break;
-      default:
-        res.writeHead(404);
-        res.write("Shit....");
-        res.end();
-        break;
-    }
-  })
-  .listen(8080);
+app.use(express.static('../public'))
+app.use(express.static('../node_modules'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+
+});
+
+app.listen(8080, () => console.log('Example app listening on port 8080'))
+
+
+
 
 mongoose.connect(
   "mongodb://admin:admin@ds023373.mlab.com:23373/allergikartandb",
