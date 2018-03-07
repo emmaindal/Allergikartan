0b936fd4b8233daf7c4cd9ab742cece23d7c477b
emmaindal <emma.indahl@gmail.com>
2018-03-07 20:23:02 +0100

    refactoring av ikonernas javascript

--- public/javascript/resturantchoice.js
+++ public/javascript/resturantchoice.js
@@ -1,58 +1,18 @@
 $(document).ready(function(){
-	$("#one").on('click', function(){
-		if ($("#one").hasClass('icon-disabled')) {
-			$(this).css('background', 'none');
-			$("#icon-one").css('opacity', '1');
-			$("#one").removeClass('icon-disabled');
-
-		} else if ($("#one").hasClass('icon-disabled') === false) {
-			$("#one").addClass('icon-disabled');
-			$(this).css('background', 'linear-gradient(rgba(180, 180, 180, 1), rgba(180, 180, 180, 1))');
-			$("#icon-one").css('opacity', '0.20');
-		} else {
-			console.log("något är fel")
-		}
-	});
-	$("#two").on('click', function(){
-		if ($("#two").hasClass('icon-disabled')) {
-			$(this).css('background', 'none');
-			$("#icon-two").css('opacity', '1');
-			$("#two").removeClass('icon-disabled');
-
-		} else if ($("#two").hasClass('icon-disabled') === false) {
-			$("#two").addClass('icon-disabled');
-			$(this).css('background', 'linear-gradient(rgba(180, 180, 180, 1), rgba(180, 180, 180, 1))');
-			$("#icon-two").css('opacity', '0.20');
-		} else {
-			console.log("något är fel")
-		}
-	});
-	$("#three").on('click', function(){
-		if ($("#three").hasClass('icon-disabled')) {
-			$(this).css('background', 'none');
-			$("#icon-three").css('opacity', '1');
-			$("#three").removeClass('icon-disabled');
-
-		} else if ($("#three").hasClass('icon-disabled') === false) {
-			$("#three").addClass('icon-disabled');
-			$(this).css('background', 'linear-gradient(rgba(180, 180, 180, 1), rgba(180, 180, 180, 1))');
-			$("#icon-three").css('opacity', '0.20');
-		} else {
-			console.log("något är fel")
-		}
-	});
-	$("#four").on('click', function(){
-		if ($("#four").hasClass('icon-disabled')) {
-			$(this).css('background', 'none');
-			$("#icon-four").css('opacity', '1');
-			$("#four").removeClass('icon-disabled');
-
-		} else if ($("#four").hasClass('icon-disabled') === false) {
-			$("#four").addClass('icon-disabled');
-			$(this).css('background', 'linear-gradient(rgba(180, 180, 180, 1), rgba(180, 180, 180, 1))');
-			$("#icon-four").css('opacity', '0.20');
-		} else {
-			console.log("något är fel")
-		}
+	var icon = $(".icons").children();
+	icon.each(function(index){
+		$('#'+index).on('click', function(){
+			if ($('#'+index).hasClass('icon-disabled')) {
+				$(this).css('background', 'none');
+				$('#icon-'+index).css('opacity', '1');
+				$('#'+index).removeClass('icon-disabled');
+			} else if ($('#'+index).hasClass('icon-disabled') === false) {
+				$('#'+index).addClass('icon-disabled');
+				$(this).css('background', 'linear-gradient(rgba(180, 180, 180, 1), rgba(180, 180, 180, 1))');
+				$('#icon-'+index).css('opacity', '0.20');
+			} else {
+				console.log("något är fel")
+			};
+		});
 	});
 });
