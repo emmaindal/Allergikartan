b735f40902f385ee6e087bfb75727c2fd5e8e03a
André Rosengren <andree_@hotmail.com>
2018-03-09 11:37:17 +0100

    Ändrat funktionsnamn från "hej" till "fileReader" i "csv-reader.js".  Lagt till kommentarer i koden. Filen kommer att krascha när den körs för den saknar felhantering. Det innebär att csv-filer som matas in måste vara perfekta för models som används. I detta fallet så matas bara de två första raderna i CSV-filen upp i databasen

---
+++ models/csv-reader.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+const csv = require("fast-csv");
+const Restaurant = require("./restaurant.js");
+// The model the csv i mapped on to
+
+mongoose.connect(
+  "mongodb://admin:admin@ds023373.mlab.com:23373/allergikartandb",
+  function(error) {
+    if (!error) {
+      console.log("databas funkar");
+      fileReader();
+    }
+  }
+);
+
+
+function fileReader() {
+  let csvStream = csv.fromPath("Restauranter.csv").on("data", function(record) {
+    //the file you want to read from
+    csvStream.pause();
+    //stops stream after each row
+    let stringToSplit = record[0];
+    splittedString = stringToSplit.split(";");
+    Restaurant.create(
+      //maps each value to the model. 
+      {
+        name: splittedString[0],
+        address: splittedString[1],
+        city: splittedString[2],
+        gluten: splittedString[3],
+        lactose: splittedString[4],
+        nut: splittedString[5],
+        vegan: splittedString[6],
+        vegetarian: splittedString[7]
+      },
+      function(err, small) {
+        if (err) {
+          console.log(err);
+        }
+
+      }
+    );
+  csvStream.resume();
+  //resumes stream with next row in csv-file.
+  });
+}
