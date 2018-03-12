fa0922cad8e830f43137a76edf397669e37acd50
olssoncharlie <charlie-112@hotmail.com>
2018-03-12 13:52:41 +0100

    - POST route for /about skickar kontaktemail

--- server.js
+++ server.js
@@ -29,6 +29,49 @@ app.get('/about', function(req, res) {
 	res.sendFile(path.join(__dirname,'/public/kontakt.html'));
 });
 
+app.post('/about', function(req, res) {
+  var FName = req.body.first_name; //input from first_name
+  var LName = req.body.last_name; //input from last_name 
+  var Email = req.body.email; // input from email
+  var MailText = req.body.mail_text;
+  
+  var auth = {
+    type: 'oauth2',
+    user: 'Allergikartan@gmail.com', // Email address to auth
+    clientId: '901695997643-qhgqk643jd3th72dqqr7u19rjhitmi5c.apps.googleusercontent.com', // Client ID from Google API center
+    clientSecret: 'BfzXQVv469R62L_ef8wZCSEf', // Client secret from google API center
+    refreshToken: '1/dG3ZAIH02K6E_HYj_sbHAYcu1MQROdQ77zdznku-XTs', // refreshes the tokens, OAUTH playground google, this is not secured, we need to make it only go to allergikartan when the server is live.
+  };
+
+  var mailOptions = {
+    from: 'Allergikartan@gmail.com',
+    to: 'Allergikartan@gmail.com',
+    subject: 'KONTAKT',
+    text: `${MailText}`,
+    html: `<p> <strong>Från:</strong> ${FName} ${LName} </p>
+          <p> ===========================================</p>
+          <p> <strong>Email:</strong> ${Email} </p>
+          <p> ===========================================</p>
+          <p> <strong>Mailtext:</strong> <br>${MailText}</p>
+          <p> ============================================</p>`
+  };
+  // Creates a transport protocoll for gmail
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: auth,
+  });
+  // Sends the mail through the transporter
+  transporter.sendMail(mailOptions, (err, res) => {
+    if (err) {
+        // Here we should enter a "mail was not sent" page
+        console.log(err);  
+    } else {
+        // Here we should render a "mail was sent page"
+        console.log(JSON.stringify(res));
+    }
+  });
+})
+
 app.post('/tipsa', function(req, res) {
   var FName = req.body.first_name; //input from first_name :::: NOT USED RIGHT NOW ::::
   var LName = req.body.last_name; //input from last_name :::: NOT USED RIGHT NOW ::::
