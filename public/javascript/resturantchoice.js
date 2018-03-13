33b2e94de85356792707e980f90f8cf6a3394f3c
olssoncharlie <charlie-112@hotmail.com>
2018-03-13 10:45:13 +0100

    - Hitta restaurang knappen visible/invisible

--- public/javascript/resturantchoice.js
+++ public/javascript/resturantchoice.js
@@ -38,3 +38,26 @@ $(document).ready(function(){
 
 	});
 });
+
+
+$('.allergi').on('click', function()  {
+	
+	if($(this).hasClass('active') == false) {
+	// If the button clicked doesnt have class active (which means its been clicked on)
+		$(this).addClass('active')
+		// add the class active
+	} else {
+	// If it DOES have it
+		$(this).removeClass('active')
+		// We remove the class (since its been clicked twice)
+	}
+	if($('.allergi').hasClass("active") == true) {
+	// If any of the allergy buttons have active (been clicked)
+		$('#search-resturant').show()
+		// We SHOW the search restaurant button
+	} else {
+	// If none of the allergies have been clicked	
+		$('#search-resturant').hide()
+		// We HIDE the search restaurant button
+	}
+})
